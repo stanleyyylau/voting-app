8410d53ed3ad69c46cae3784a93388c9b5a89491
Stanley <stanleyyylau@gmail.com>
2016-11-13 11:45:37 +0800

    finish all template logic

--- server.js
+++ server.js
@@ -7,12 +7,37 @@ app.set('view engine', 'pug');
 app.use(express.static('views'));
 
 app.get('/', function(req, res){
-  res.render('home');
+  res.render('home',{
+    pretty: true,
+    title: 'Welcome to the voting app',
+    showFeature: true
+  });
+})
+
+app.get('/signup', function(req, res){
+  var name = req.params.name;
+  res.render('home', {
+    pretty: true,
+    name,
+    signup: true,
+    title: 'create a new account'
+  });
+})
+
+app.get('/login', function(req, res){
+  var name = req.params.name;
+  res.render('home', {
+    pretty: true,
+    name,
+    login: true,
+    title: 'Login to crate a vote'
+  });
 })
 
 app.get('/:name', function(req, res){
   var name = req.params.name;
   res.render('home', {
+    pretty: true,
     name,
     newPoll: true,
     title: 'Add new poll'
@@ -22,10 +47,32 @@ app.get('/:name', function(req, res){
 app.get('/:name/all', function(req, res){
   var name = req.params.name;
   res.render('home', {
+    pretty: true,
     name,
     allPolls: true,
     title: 'All your polls'
   })
+})
+
+app.get('/:name/:vote', function(req, res){
+  var name = req.params.name;
+  var voteId = req.params.vote;
+  var showResult = req.query.r == 'true' ? true : false;
+  if(showResult){
+    res.render('home', {
+      pretty: true,
+      name,
+      title: 'This is the vote name',
+      showResult
+    })
+  }else{
+    res.render('home', {
+      pretty: true,
+      name,
+      title: 'This is the vote name',
+      voteId
+    })
+  }
 })
 
 app.listen(port, function () {
