8438b01ae696deb73b34c78ce6dd45ca5adfec5a
Stanley Lau <stanleyyylau@gmail.com>
2016-11-27 17:59:04 +0800

    finish show indivisual poll and add custom option, about to implement actually voting and show result

--- routers/routes.js
+++ routers/routes.js
@@ -2,6 +2,7 @@ var express = require("express");
 var passport = require("passport");
 
 var {User} = require("../models/user");
+var {Poll} = require("../models/poll");
 var router = express.Router();
 
 function ensureAuthenticated(req, res, next) {
@@ -89,26 +90,139 @@ router.get('/new', ensureAuthenticated, function(req, res) {
   })
 })
 
+router.post('/new', ensureAuthenticated, function(req, res){
+  // first make the options array
+  var optionsArr =[];
+  console.log(req.body);
+  var optionss = req.body["options[]"];
+  optionss.forEach(function(value, index){
+    var newObj = {
+      option: value,
+      votes: 0
+    }
+    optionsArr.push(newObj);
+  })
+  var newPoll = new Poll({
+    title: req.body.title,
+    _creator: req.user._id,
+    options: optionsArr
+  })
 
-// router.get('/:name', function(req, res){
-//   var name = req.params.name;
-//   res.render('home', {
-//     pretty: true,
-//     name,
-//     newPoll: true,
-//     title: 'Add new poll'
-//   // });
-// })
+  // save this shit to DB
+  newPoll.save().then((poll) => {
+    console.log(poll)
+    res.json({
+      redirect: '/all'
+    })
+  }, (e) => {
+    res.status(400).send(e);
+  })
+})
 
-// router.get('/:name/all', function(req, res){
-//   // var name = req.params.name;
-//   // res.render('home', {
-//   //   pretty: true,
-//   //   name,
-//   //   allPolls: true,
-//   //   title: 'All your polls'
-//   // })
-// })
+
+router.get('/allpolls', function(req, res){
+
+  Poll.find({}).then((all) => {
+    console.log(all);
+    res.render('all_polls', {
+      pretty: true,
+      title: 'All the polls',
+      polls: all
+    })
+  }, (e) => {
+    console.log(e);
+  })
+
+
+})
+
+
+
+
+router.get('/all', ensureAuthenticated, function(req, res){
+
+  Poll.find({_creator: req.user._id}).then((all) => {
+    res.render('all_my_polls', {
+      pretty: true,
+      allPolls: true,
+      title: 'All your polls',
+      polls: all
+    })
+  }, (e) => {
+    console.log(e);
+  })
+
+
+})
+
+router.get('/poll/:id', function(req, res){
+  var id = req.params.id;
+  var doc;
+
+  Poll.findOne({_id: id}).then((pdoc) => {
+    console.log('the doc is ....')
+    console.log(pdoc)
+    doc = pdoc;
+    return User.findOne({_id: doc._creator})
+
+  }).then((auther) => {
+    var isLogIn;
+    if (req.user){
+      isLogIn = true;
+    }else{
+      isLogIn = false;
+    }
+    res.render('poll_detail', {
+      pretty: true,
+      poll: doc,
+      isLogIn,
+      voteTitle: doc.title,
+      voteAuthor: auther.username,
+      voteOptions: doc.options
+    })
+  })
+  .catch((e) => {
+    console.log(e);
+  })
+})
+
+
+router.post('/newoption', function(req, res){
+  Poll.findOneAndUpdate(
+    {_id: req.body.voteId},
+    {$push: {"options": {option: req.body.newOption, votes: 0}}},
+    {safe: true, upsert: true}
+  ).then((poll) => {
+    console.log(poll);
+    res.json({
+      redirect: '/poll/'+ poll._id
+    })
+  }).catch((e) => {
+    console.log(e);
+  })
+})
+
+router.get('/test', function(req, res){
+    res.render('home', {
+      pretty: true,
+      name: true,
+      title: 'This is the vote name',
+      voteId: '583a4f65308c1e0df28923e5'
+    })
+})
+
+
+router.delete('/poll', ensureAuthenticated, function(req, res){
+  Poll.findOneAndRemove({_id: req.body.id}).then((doc) => {
+    console.log('the remove doc is ....')
+    console.log(doc)
+    res.json({redirect: "/all"})
+  }, (e) => {
+    console.log(e);
+  })
+})
+
+
 
 router.get('/:name/:vote', function(req, res){
   // var name = req.params.name;
